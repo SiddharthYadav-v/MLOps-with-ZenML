43c5d3f689d6a70cfd8e99fb0c0ab31275a23669
Siddharth Yadav <siddharth0208yadav@gmail.com>
2023-10-16 22:18:41 +0530

    implemented data_cleaning.py

--- steps/clean_data.py
+++ steps/clean_data.py
@@ -2,8 +2,38 @@ import logging
 
 import pandas as pd
 from zenml import step
+from src.data_cleaning import DataCleaning, DataDivideStrategy, DataPreProcessStrategy
+from typing_extensions import Annotated
+from typing import Tuple
 
 @step
-def clean_df(df: pd.DataFrame) -> pd.DataFrame:
-    pass
+def clean_df(df: pd.DataFrame) -> Tuple[
+    Annotated[pd.DataFrame, "X_train"],
+    Annotated[pd.DataFrame, "X_test"],
+    Annotated[pd.Series, "y_test"],
+    Annotated[pd.Series, "y_test"],
+]:
+    """
+    Cleans the data and divides it into train and test
 
+    Args:
+        df: Raw data
+    Returns:
+        X_train: training data
+        X_test: testing data
+        y_train: train labels
+        y_test: test labels
+    """
+    try:
+        process_strategy = DataPreProcessStrategy()
+        data_cleaning = DataCleaning(df, process_strategy)
+        processed_data = data_cleaning.handle_data()
+
+        divide_strategy = DataDivideStrategy()
+        data_cleaning = DataCleaning(processed_data, divide_strategy)
+        X_train, X_test, y_train, y_test = data_cleaning.handle_data()
+        logging.info("Data cleaning completed")
+    except Exception as e:
+        logging.error(f"Error in cleaning data: {e}")
+        raise e
+    
--- steps/model_train.py
+++ steps/model_train.py
@@ -5,4 +5,10 @@ from zenml import step
 
 @step
 def train_model(df: pd.DataFrame) -> None:
+    """
+    Trains the model on the ingested data
+
+    Args:
+        df: the ingested data
+    """
     pass
